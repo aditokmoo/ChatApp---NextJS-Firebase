32afe5b2452efba17417e29f3a7d03fcca136c1d
Havock48 <adit.github@gmail.com>
2024-08-04 17:47:54 +0200

    overflow on chat list added

--- app/dashboard/chat/_components/List.tsx
+++ app/dashboard/chat/_components/List.tsx
@@ -31,12 +31,40 @@ const userData = [
         name: 'John Doe',
         message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia.",
         lastMessageTime: '19:48'
+    },
+    {
+        id: 5,
+        avatar: <FaUser />,
+        name: 'John Doe',
+        message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia.",
+        lastMessageTime: '19:48'
+    },
+    {
+        id: 6,
+        avatar: <FaUser />,
+        name: 'John Doe',
+        message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia.",
+        lastMessageTime: '19:48'
+    },
+    {
+        id: 7,
+        avatar: <FaUser />,
+        name: 'John Doe',
+        message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia.",
+        lastMessageTime: '19:48'
+    },
+    {
+        id: 8,
+        avatar: <FaUser />,
+        name: 'John Doe',
+        message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia.",
+        lastMessageTime: '19:48'
     }
 ]
 
 export default function List() {
     return (
-        <div className="flex flex-col w-1/3 bg-rgba-dark rounded-tr-xl rounded-br-xl p-12">
+        <div className="flex flex-col w-1/3 bg-rgba-dark rounded-tr-xl rounded-br-xl p-12 h-full overflow-y-auto">
             <div className="flex relative">
                 <FaSearch className='absolute top-5 left-4 text-white' />
                 <input id='search' name='search' placeholder='Search...' className='p-4 pl-12 rounded-md bg-light-dark border border-dark w-full' />
