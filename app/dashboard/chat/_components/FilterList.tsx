f7707649cd8207cb0744b6c6f737f339650e4bb0
Havock48 <adit.github@gmail.com>
2024-08-05 12:54:50 +0200

    added new font poppins

--- app/dashboard/chat/_components/FilterList.tsx
+++ app/dashboard/chat/_components/FilterList.tsx
@@ -7,7 +7,7 @@ export default function FilterList() {
         <div className="bg-dark z-40 border-b border-light-dark text-white p-4 sticky top-0 left-0 w-full">
             <div className="flex justify-between items-center">
                 <Link href='/' className='text-sm flex items-center gap-2'><span className='bg-primary flex justify-center items-center shadow-lg w-6 h-6 font-semibold rounded-full text-xs'>2</span> Message request</Link>
-                <button className='flex items-center gap-2 text-xs'><FaPlus />New group</button>
+                <button className='flex items-center gap-2 text-xs font-semibold'><FaPlus />New group</button>
             </div>
         </div>
     )
