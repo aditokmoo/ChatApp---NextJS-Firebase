c14dd44bced253897e8db2970340ee525bb56f36
Havock48 <adit.github@gmail.com>
2024-08-19 22:36:49 +0200

    added functionality for creating new chat

--- app/dashboard/_components/UserList.tsx
+++ app/dashboard/_components/UserList.tsx
@@ -1,13 +1,68 @@
 'use client'
 import useGetAllUsers from '@/hooks/useUser'
+import { db } from '@/lib/firebase';
+import { useUserStore } from '@/zustand/userStore';
+import { arrayUnion, collection, doc, getDoc, serverTimestamp, setDoc, updateDoc } from 'firebase/firestore';
 import Image from 'next/image';
-import React from 'react'
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react'
 import { FaSearch } from 'react-icons/fa';
 
 export default function UserList() {
     const { data, isLoading } = useGetAllUsers();
+    const { currentUser } = useUserStore();
+    const router = useRouter();
 
     if (isLoading) return <div>Loading...</div>
+
+    const handleStartChat = async (user) => {
+        const chatRef = collection(db, 'chats');
+        const userChatsRef = collection(db, 'userChats');
+    
+        try {
+            // Create a unique chat ID based on user IDs
+            const chatId = currentUser?.id < user?.id
+                ? `${currentUser?.id}_${user?.id}`
+                : `${user?.id}_${currentUser?.id}`;
+    
+            const existingChatDoc = await getDoc(doc(chatRef, chatId));
+    
+            if (existingChatDoc.exists()) {
+                // If chat already exists, navigate to it
+                router.push(`/dashboard/chat`);
+            } else {
+                // Create a new chat
+                await setDoc(doc(chatRef, chatId), {
+                    createdAt: serverTimestamp(),
+                    messages: []
+                });
+    
+                // Update user data that is getting the message
+                await updateDoc(doc(userChatsRef, user?.id), {
+                    chats: arrayUnion({
+                        chatId,
+                        lastMessage: '',
+                        receiverId: currentUser?.id,
+                        updatedAt: Date.now()
+                    })
+                });
+    
+                // Update user that is sending the message
+                await updateDoc(doc(userChatsRef, currentUser?.id), {
+                    chats: arrayUnion({
+                        chatId,
+                        lastMessage: '',
+                        receiverId: user?.id,
+                        updatedAt: Date.now()
+                    })
+                });
+    
+                router.push(`/dashboard/chat`);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     console.log(data)
     return (
@@ -30,15 +85,15 @@ export default function UserList() {
                 <FaSearch className='absolute left-[35px] text-gray-400' />
                 <input type="text" placeholder='Search...' className='w-full p-4 text-sm bg-dark rounded-sm pl-12' />
             </div>
-            {data?.map((item) => (
-                <div className="w-full flex gap-3 items-center odd:bg-dark p-4 text-white cursor-pointer hover:opacity-80">
+            {data?.map((user) => (
+                <div className="w-full flex gap-3 items-center odd:bg-dark p-4 text-white cursor-pointer hover:opacity-80" onClick={() => handleStartChat(user)}>
                     <div className="relative">
-                        <Image src={item.avatar} alt="avatar" width={50} height={50} className='rounded-full' />
+                        <Image src={user.avatar} alt="avatar" width={50} height={50} className='rounded-full' />
                         <div className="absolute bottom-[4px] right-[3px] shadow-lg w-2 h-2 bg-green rounded-full"></div>
                     </div>
                     <div className="flex flex-col">
-                        <span>{item.username}</span>
-                        <span className='text-gray-500 text-sm'>{item.email}</span>
+                        <span>{user.username}</span>
+                        <span className='text-gray-500 text-sm'>{user.email}</span>
                     </div>
                 </div>
             ))}
